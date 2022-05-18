fe3fdb429d2829041bdff0a4dbf50f1b21c5391c
OmarAyman415 <o.ayman415@gmail.com>
2022-05-18 05:04:18 +0200

    empty project

--- Insta/Insta/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Insta.MvcApplication" Language="C#" %>
--- Insta/Insta/ViewModels/LoginRegisterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Insta.ViewModels
-{
-    public class LoginRegisterViewModel
-    {
-        public RegisterViewModel registerModel { get; set; }
-        public LoginViewModel loginModel { get; set; }
-
-    }
-}
