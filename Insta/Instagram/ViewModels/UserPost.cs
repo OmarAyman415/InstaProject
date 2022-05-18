df3732b2e5acb0a9f4065601533966eca26d0b71
Amr Mostafa <amr.mos.2001@gmail.com>
2022-05-18 05:20:41 +0200

    AddViewModel

---
+++ Insta/Instagram/ViewModels/UserPost.cs
@@ -0,0 +1,12 @@
+﻿using Instagram.Models;
+
+namespace Instagram.ViewModels
+{
+    public class UserPost
+    {
+        public User user { get; set; }
+
+        public Post post { get; set; }
+
+    }
+}
