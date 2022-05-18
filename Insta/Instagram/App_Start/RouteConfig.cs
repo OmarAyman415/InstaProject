f7aef967117af944b503a0bcea15093ab1b70e33
OmarAyman415 <o.ayman415@gmail.com>
2022-05-18 05:08:07 +0200

    Added config files

---
+++ Insta/Instagram/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Instagram
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Security", action = "Login", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ Insta/Instagram/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using Instagram.App_Start;
+using Instagram.Services;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Instagram
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            var container = new Container();
+
+            container.Register<Service>();
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            GlobalFilters.Filters.Add(new AuthorizationFilter());
+        }
+    }
+}
