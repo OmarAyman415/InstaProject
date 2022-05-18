b67813a61f2024988fb4173892b4e8ce70faef8e
omar1999465 <80550459+omar1999465@users.noreply.github.com>
2022-05-18 05:17:29 +0200

    add Controllers

---
+++ Insta/Instagram/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Instagram.Models;
+using Instagram.Services;
+using Instagram.ViewModels;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+namespace Instagram.Controllers
+{
+    public class HomeController : Controller
+    {
+        Service _service = null;
+
+        public HomeController(Service service)
+        {
+            _service = service;
+        }
+        [Authorize]
+        public ActionResult Profile(string user)
+        {
+            //Prevent users from accessing other's profiles
+            if (Session["UserName"].Equals(user))
+            {
+                User userProfile = new User();
+                List<Friend> userFriends = new List<Friend>();
+
+                userProfile = _service.userService.GetUserByEmail(user);
+                userFriends = _service.friendCondition.GetFriends(userProfile.UserId);
+
+
+                UserFriendViewModel userFriend = new UserFriendViewModel()
+                {
+                    user = userProfile,
+                    friends = userFriends
+                };
+                return View(userFriend);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Security");
+            }
+        }
+
+
+        [Authorize]
+        public ActionResult EditProfile(string user)
+        {
+            //Prevent users from accessing other's profiles
+            if (Session["UserName"].Equals(user))
+            {
+                User userProfile = new User();
+                userProfile = _service.userService.GetUserByEmail(user);
+                return View(userProfile);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Security");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult EditProfile(User user, HttpPostedFileBase file)
+        {
+            User userLogged = _service.userService.GetUserByEmail(Session["UserName"].ToString());
+
+            if (file != null)
+            {
+                //Save file on the server
+                file.SaveAs(HttpContext.Server.MapPath("~/Content/images/profileImage/") + file.FileName);
+                //set photo name with file name
+                user.ProfileImage = file.FileName;
+            }
+
+
+            user.Username = userLogged.Username;
+            user.UserId = userLogged.UserId;
+
+            _service.userService.EditUser(user);
+
+            //Prevent users from accessing other's profiles
+            return RedirectToAction("Profile", "Home", new { user = user.Email });
+        }
+
+    }
+}
---
+++ Insta/Instagram/Controllers/PostController.cs
@@ -0,0 +1,127 @@
+﻿using Instagram.Models;
+using Instagram.Services;
+using Instagram.ViewModels;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Instagram.Controllers
+{
+    public class PostController : Controller
+    {
+        static int userIDGlobal;
+        Service _service = null;
+
+        public PostController(Service service)
+        {
+            _service = service;
+        }
+
+        [Authorize]
+        // GET: Post
+        public ActionResult Post(int id)
+        {
+            if (Session["UserID"].Equals(id))
+            {
+                ViewBag.ID = id;
+                userIDGlobal = id;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Security");
+            }
+        }
+
+
+
+        //add post
+        [HttpPost]
+        public ActionResult Post(Post postSent, HttpPostedFileBase file, string captionText)
+        {
+
+            if (file != null)// if user send an image, save it in the server 
+            {
+                file.SaveAs(HttpContext.Server.MapPath("~/Content/images/") + file.FileName);
+                Post post = new Post()
+                {
+                    UserId = userIDGlobal,
+                    photo = file.FileName,
+                    caption = captionText
+                };
+
+
+                _service.postService.AddPost(post);
+
+                string userEmail = _service.userService.GetUserEmail(userIDGlobal);
+
+                return RedirectToAction("Profile", "Home", new { user = userEmail });
+            }
+            return View(userIDGlobal);
+        }
+
+        public ActionResult ViewPost(int userId, int postId)
+        {
+            User userData = _service.userService.GetUserById(userId);
+            Post postData = _service.postService.GetPost(postId);
+
+            UserPost userPost = new UserPost()
+            {
+                user = userData,
+                post = postData
+            };
+            return View(userPost);
+        }
+        [HttpGet]
+        public ActionResult AddComment(int userCommentedId, int postId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(int userCommentedId, int postId, string comments)
+        {
+            var x = comments;
+
+            Comment comment = new Comment()
+            {
+                PostId = postId,
+                UserCommmentedId = userCommentedId,
+                CommentText = comments,
+                CommentDate = System.DateTime.Now
+            };
+            _service.commentService.AddCommnet(comment);
+            return RedirectToAction("ViewPost", "Post", new { userId = userCommentedId, postId = postId });
+        }
+
+        public ActionResult AddLikeOrDislike(int userId, int postId, bool LikeState)
+        {
+            React newReact = new React()
+            {
+                PostId = postId,
+                UserReactedId = userId,
+                ReactState = LikeState,
+            };
+            React oldReact = _service.reactService.GetReact(userId, postId);
+            if (oldReact != null)
+            {
+                if (!oldReact.ReactState.Equals(newReact.ReactState))
+                {
+                    _service.postService.DecrementPostReact(postId, oldReact.ReactState);
+                    _service.postService.IncrementPostReact(postId, newReact.ReactState);
+
+                    // Revert React State in Database
+                    _service.reactService.UpdateReact(oldReact);
+                }
+            }
+            else
+            {
+                _service.postService.IncrementPostReact(postId, newReact.ReactState);
+                _service.reactService.AddReact(newReact);
+            }
+
+            return RedirectToAction("ViewPost", "Post", new { userId = userId, postId = postId });
+
+        }
+
+    }
+}
