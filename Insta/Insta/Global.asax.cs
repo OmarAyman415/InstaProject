434fbafc118f666aa104f8e24417129999a0cb89
OmarAyman415 <o.ayman415@gmail.com>
2022-05-18 04:57:19 +0200

    Update Global.asax.cs

--- Insta/Insta/Global.asax.cs
+++ Insta/Insta/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Insta
@@ -13,6 +9,14 @@ namespace Insta
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            var container = new Container();
+
+            container.Register<Service>();
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            GlobalFilters.Filters.Add(new AuthorizationFilter());
         }
     }
 }
